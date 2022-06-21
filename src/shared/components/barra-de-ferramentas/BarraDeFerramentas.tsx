a58ab94eec7e284b2cce7f9551ee8bb0d80a5812
Daniel da Silva Filho <danielbrumdasilva@gmail.com>
2022-06-21 20:24:13 -0300

    feat: Adicionado barra de ferramentas da listagem.

---
+++ src/shared/components/barra-de-ferramentas/BarraDeFerramentas.tsx
@@ -0,0 +1,77 @@
+import { AccountCircle } from '@mui/icons-material'
+import {
+  Box,
+  Button,
+  Icon,
+  InputAdornment,
+  Paper,
+  TextField,
+  useTheme
+} from '@mui/material'
+
+interface IBarraDeFerramentasProps {
+  textoDaBusca?: string,
+  mostrarInputBusca?: boolean,
+  aoMudarTextoDeBusca?: (novoTexto: string) => void,
+  textoBotaoNovo?: string,
+  mostrarBotaoNovo?: boolean,
+  aoClicarEmNovo?: () => void,
+}
+
+export const BarraDeFerramentas: React.FC<IBarraDeFerramentasProps> = ({ 
+  textoDaBusca = '',
+  mostrarInputBusca = false,
+  aoMudarTextoDeBusca,
+  aoClicarEmNovo,
+  textoBotaoNovo = 'Novo',
+  mostrarBotaoNovo = true,
+}) => {
+  const theme = useTheme()
+
+  return(
+    <Box
+      gap={1}
+      marginX={1}
+      padding={1}
+      paddingX={2}
+      display="flex"
+      alignItems="center"
+      height={theme.spacing(5)}
+      component={Paper}
+    > 
+      {mostrarInputBusca && (
+        <TextField 
+          size="small"
+          value={textoDaBusca}
+          onChange={(e) => aoMudarTextoDeBusca?.(e.target.value)}
+          placeholder="Pesquisar..."
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <Icon>search</Icon>
+              </InputAdornment>
+            ),
+          }}
+        />
+      )}
+
+      <Box
+        flex={1}
+        display="flex"
+        justifyContent="end"
+      >
+        {mostrarBotaoNovo && (
+          <Button
+            color="primary"
+            disableElevation
+            variant="contained"
+            startIcon={<Icon>add</Icon>}
+            onClick={aoClicarEmNovo}
+          >
+            {textoBotaoNovo}
+          </Button>
+        )}
+      </Box>
+    </Box>
+  )
+}
--- src/shared/components/menu-lateral/MenuLateral.tsx
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -56,7 +56,7 @@ export const MenuLateral: React.FC = ({ children }) => {
         <Box width={theme.spacing(28)} height="100%" display="flex" flexDirection="column">
           
           <Box width="100%" height={theme.spacing(20)} display="flex" alignItems="center" justifyContent="center">
-            <Avatar sx={{ height: theme.spacing(12), width: theme.spacing(12) }} src="https://scontent.frvy1-1.fna.fbcdn.net/v/t39.30808-6/273207314_1205759309827923_651391042645074167_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=aicVRw_GfwgAX_dFdO5&tn=xsVMt3kD78h9xU5_&_nc_ht=scontent.frvy1-1.fna&oh=00_AT_nJxWuWRTsO9Gfo09gU_WmQSpl43XHmywDthOHPrVoxw&oe=62B10CC3"/>
+            <Avatar sx={{ height: theme.spacing(12), width: theme.spacing(12) }} src="https://scontent.frvy1-1.fna.fbcdn.net/v/t39.30808-6/273207314_1205759309827923_651391042645074167_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=LtFlku7UPboAX_vnGEE&tn=xsVMt3kD78h9xU5_&_nc_ht=scontent.frvy1-1.fna&oh=00_AT8wUfEJ0Ol0E2TlYcZq08ANiybYB_CHQHhDklC9PbZ98A&oe=62B6FB83"/>
           </Box>
           
           <Divider />
