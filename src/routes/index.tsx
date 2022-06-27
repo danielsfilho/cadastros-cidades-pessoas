5f96a274bdd5f6437609186e660f0b43e33289a6
Daniel da Silva Filho <danielbrumdasilva@gmail.com>
2022-06-27 19:38:58 -0300

    feat: Criando tela de cidades.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,9 @@
 import { useEffect } from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
-import { Dashboard } from '../pages'
+import {
+  Dashboard,
+  ListagemDeCidade
+} from '../pages'
 import { useDrawerContext } from '../shared/contexts'
 
 export const AppRoutes = () => {
@@ -13,12 +16,20 @@ export const AppRoutes = () => {
         path: '/pagina-inicial',
         label: 'Página inicial',
       },
+      {
+        icon: 'location_city',
+        path: '/cidades',
+        label: 'Cidades',
+      },
     ])
   }, [])
   
   return  (
     <Routes>
       <Route path="/pagina-inicial" element={<Dashboard />} />
+      
+      <Route path="/cidades" element={<ListagemDeCidade />} />
+      {/*<Route path="/cidades/detalhe/:id" element={<ListagemDeCidade />} />*/}
       
       <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
