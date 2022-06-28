3aa17fda20bf5b8d0f0d008a60a37a3af08c8ee9
Daniel da Silva Filho <danielbrumdasilva@gmail.com>
2022-06-27 21:10:57 -0300

    feat: Adicionado useDebounce.

--- src/pages/pessoas/ListagemDePessoas.tsx
+++ src/pages/pessoas/ListagemDePessoas.tsx
@@ -1,11 +1,14 @@
 import { useEffect, useMemo } from 'react'
 import { useSearchParams } from 'react-router-dom'
 import { FerramentasDaListagem } from '../../shared/components'
+import { useDebounce } from '../../shared/hooks'
 import { LayoutBaseDePagina } from '../../shared/layouts'
 import { PessoasService } from '../../shared/services/api/pessoas/PessoasService'
 
 export const ListagemDePessoas: React.FC = () => {
   const [searchParams, setSearchParams] = useSearchParams()
+
+  const { debounce } = useDebounce(3000)
 
   const busca = useMemo(() => {
     return searchParams.get('busca') || ''
@@ -13,15 +16,18 @@ export const ListagemDePessoas: React.FC = () => {
 
   useEffect(() => {
 
-    PessoasService.getAll(1, busca)
-      .then((result) => {
-        if(result instanceof Error) {
-          alert(result.message)
-        }else {
-          console.log(result)
-        }
-      })
+    debounce(() => {
+      PessoasService.getAll(1, busca)
+        .then((result) => {
+          if(result instanceof Error) {
+            alert(result.message)
+          }else {
+            console.log(result)
+          }
+        })
+    })
   }, [busca])
+    
 
   return (
     <LayoutBaseDePagina
