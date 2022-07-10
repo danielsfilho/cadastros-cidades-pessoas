1d0a379621a9a53c32664b025d794c67e31e6259
Daniel da Silva Filho <danielbrumdasilva@gmail.com>
2022-07-10 17:27:12 -0300

    feat: Criado crud completo de cidades.

--- src/pages/cidades/ListagemDeCidades.tsx
+++ src/pages/cidades/ListagemDeCidades.tsx
@@ -15,18 +15,18 @@ import {
 } from '@mui/material'
 import { useNavigate, useSearchParams } from 'react-router-dom'
 
-import { IListagemPessoa, PessoasService } from '../../shared/services/api/pessoas/PessoasService'
+import { IListagemCidade, CidadesService } from '../../shared/services/api/cidades/CidadesService'
 import { FerramentasDaListagem } from '../../shared/components'
 import { LayoutBaseDePagina } from '../../shared/layouts'
 import { useDebounce } from '../../shared/hooks'
 import { Environment } from '../../shared/environment'
 
-export const ListagemDePessoas: React.FC = () => {
+export const ListagemDeCidades: React.FC = () => {
   const [searchParams, setSearchParams] = useSearchParams()
   const { debounce } = useDebounce()
   const navigate = useNavigate()
 
-  const [rows, setRows] = useState<IListagemPessoa[]>([])
+  const [rows, setRows] = useState<IListagemCidade[]>([])
   const [isLoading, setIsLoading] = useState(true)
   const [totalCount, setTotalCount] = useState(0)
 
@@ -42,7 +42,7 @@ export const ListagemDePessoas: React.FC = () => {
     setIsLoading(true)
 
     debounce(() => {
-      PessoasService.getAll(pagina, busca)
+      CidadesService.getAll(pagina, busca)
         .then((result) => {
           setIsLoading(false)
 
@@ -60,7 +60,7 @@ export const ListagemDePessoas: React.FC = () => {
     
   const handleDelete = (id: number) => {
     if(confirm('Realmente deseja apagar?')) {
-      PessoasService.deleteById(id)
+      CidadesService.deleteById(id)
         .then(result => {
           if(result instanceof Error) {
             alert(result.message)
@@ -79,13 +79,13 @@ export const ListagemDePessoas: React.FC = () => {
 
   return (
     <LayoutBaseDePagina
-      titulo="Listagem de pessoas"
+      titulo="Listagem de cidades"
       barraDeFerramentas={
         <FerramentasDaListagem
           mostrarInputBusca
           textoDaBusca={busca}
           textoBotaoNovo="Nova"
-          aoClicarEmNovo={() => navigate('/pessoas/detalhe/nova')}
+          aoClicarEmNovo={() => navigate('/cidades/detalhe/nova')}
           aoMudarTextoDeBusca={(texto) =>
             setSearchParams({ busca: texto, pagina: '1' }, { replace: true })
           }
@@ -97,8 +97,7 @@ export const ListagemDePessoas: React.FC = () => {
           <TableHead>
             <TableRow>
               <TableCell width={100}>Ações</TableCell>
-              <TableCell>Nome completo</TableCell>
-              <TableCell>Email</TableCell>
+              <TableCell>Nome</TableCell>
             </TableRow>
           </TableHead>
 
@@ -109,12 +108,11 @@ export const ListagemDePessoas: React.FC = () => {
                   <IconButton size="small" onClick={() => handleDelete(row.id)}>
                     <Icon>delete</Icon>
                   </IconButton>
-                  <IconButton size="small" onClick={() => navigate(`/pessoas/detalhe/${row.id}`)}>
+                  <IconButton size="small" onClick={() => navigate(`/cidades/detalhe/${row.id}`)}>
                     <Icon>edit</Icon>
                   </IconButton>
                 </TableCell>
-                <TableCell>{row.nomeCompleto}</TableCell>
-                <TableCell>{row.email}</TableCell>
+                <TableCell>{row.nome}</TableCell>
               </TableRow>
             ))}           
           </TableBody>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,7 +3,9 @@ import { Routes, Route, Navigate } from 'react-router-dom'
 import {
   Dashboard,
   ListagemDePessoas,
-  DetalheDePessoas
+  DetalheDePessoas,
+  ListagemDeCidades,
+  DetalheDeCidades
 } from '../pages'
 import { useDrawerContext } from '../shared/contexts'
 
@@ -16,6 +18,11 @@ export const AppRoutes = () => {
         icon: 'home',
         path: '/pagina-inicial',
         label: 'Página inicial',
+      },
+      {
+        icon: 'location_city',
+        path: '/cidades',
+        label: 'Cidades',
       },
       {
         icon: 'people',
@@ -31,6 +38,9 @@ export const AppRoutes = () => {
 
       <Route path="/pessoas" element={<ListagemDePessoas />} />
       <Route path="/pessoas/detalhe/:id" element={<DetalheDePessoas />} />
+
+      <Route path="/cidades" element={<ListagemDeCidades />} />
+      <Route path="/cidades/detalhe/:id" element={<DetalheDeCidades />} />
 
       <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
